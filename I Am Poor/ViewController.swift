c3644d5c9c1a12d39cfd4c23ff525294856332b6
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-07-30 21:41:40 +0300

    I Am Poor
    Storyboard and Interface Builder

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Maxim Bekmetov on 21.07.2020.
+//  Copyright © 2020 Maxim Bekmetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
